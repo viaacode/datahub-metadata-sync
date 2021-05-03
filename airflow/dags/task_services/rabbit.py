cf7e556153096442f868c160d6e956f98ee95f7c
Walter Schreppers <schreppers@gmail.com>
2021-05-03 17:46:04 +0200

    make rabbit client configurable with env vars.

--- airflow/dags/task_services/rabbit.py
+++ airflow/dags/task_services/rabbit.py
@@ -3,19 +3,19 @@
 
 import time
 import pika
-
+import os
 
 class RabbitClient:
     def __init__(self):
         configParser = ConfigParser()
         self.rabbitConfig = configParser.app_cfg["rabbitmq"]
 
-        self.RABBIT_USER = 'todo'
-        self.RABBIT_PASS = 'todo'
-        self.RABBIT_HOST = 'todo'
-        self.RABBIT_PORT = 'todo'
-        self.RABBIT_QUEUE = 'todo'
-        self.RABBIT_PREFETCH = 1
+        self.RABBIT_USER = os.environ.get('RABBIT_USER', 'some_rmq_user')
+        self.RABBIT_PASS = os.environ.get('RABBIT_PASS', 'some_rmq_passw')
+        self.RABBIT_HOST =os.environ.get('RABBIT_HOST', 'rmq_host_here')
+        self.RABBIT_PORT = os.environ.get('RABBIT_USER', 'rmq_port_here')
+        self.RABBIT_QUEUE = os.environ.get('RABBIT_QUEUE', 'rmq_queue_name_here')
+        self.prefetch_count = int(os.environ.get('RABBIT_PREFETCH', '1'))
 
         self.credentials = pika.PlainCredentials(
             self.RABBIT_USER, self.RABBIT_PASS
@@ -30,7 +30,6 @@ class RabbitClient:
         )
 
         self.channel = self.connection.channel()
-        self.prefetch_count = int(self.RABBIT_PREFETCH)
 
     def send_message(self, routing_key, body, exchange=""):
         try:
