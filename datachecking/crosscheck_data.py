cd402feff6903c011aa5fa29a79751ca4443a735
Walter Schreppers <schreppers@gmail.com>
2021-05-20 11:27:04 +0200

    parse cmd line so script can be used for multiple csv's

--- datachecking/crosscheck_data.py
+++ datachecking/crosscheck_data.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import psycopg2
 
@@ -30,22 +31,32 @@ def check_fragment(cursor, row):
         print(f"{fragment_id} NOT FOUND: row={row}")
 
 
-database = psycopg2.connect(
-    dbname="airflow_development",
-    user="postgres",
-    password="postgres",
-    host="127.0.0.1"
+if __name__ == '__main__':
 
-)
-cursor = database.cursor()
+    if len(sys.argv)<2:
+        print(f"USAGE: python {sys.argv[0]} <csv file to check>")
+        sys.exit(1)
 
-# with open('AIF_QAS_20210512.csv', newline='') as csvfile:
-with open('aif_production.csv', newline='') as csvfile:
-    csvdata = csv.reader(csvfile, delimiter=';', quotechar='"')
-    for row in csvdata:
-        check_fragment(cursor, row)
+    # 'AIF_QAS_20210512.csv'
+    # 'aif_production.csv'
+    csv_file = sys.argv[1]
+
+    database = psycopg2.connect(
+        dbname="airflow_development",
+        user="postgres",
+        password="postgres",
+        host="127.0.0.1"
+
+    )
+    cursor = database.cursor()
+
+    with open(csv_file, newline='') as csvfile:
+        csvdata = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for row in csvdata:
+            #check_external_id(cursor, row)
+            check_fragment(cursor, row)
 
 
-cursor.close()
-database.close()
+    cursor.close()
+    database.close()
 
